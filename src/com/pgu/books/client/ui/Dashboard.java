651b72de6077f9428f83fb5cac25b38d6c708860
Guilcher Pascal <pgu.dev@gmail.com>
2012-04-04 21:17:35 +0200

    ..

--- src/com/pgu/books/client/ui/Dashboard.java
+++ src/com/pgu/books/client/ui/Dashboard.java
@@ -5,6 +5,7 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -20,31 +21,43 @@ public class Dashboard extends Composite {
     interface DashboardUiBinder extends UiBinder<Widget, Dashboard> {
     }
 
+    @UiField
+    MenuAdmin menuAdmin;
+
+    @UiField
+    HTMLPanel graphs;
+
     @UiField(provided = true)
     DockPanel dashboard;
 
     public Dashboard() {
-        dashboard = new DockPanel();
-        dashboard.setSpacing(4);
-        dashboard.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-
-        dashboard.add(new MenuAdmin(), DockPanel.NORTH);
-        dashboard.add(new Menu(), DockPanel.NORTH);
-        dashboard.add(new Filters(), DockPanel.WEST);
-
-        // final HTML contents = new HTML("center");
-        final BookBoard bookBoard = new BookBoard();
-        final ScrollPanel scroller = new ScrollPanel(bookBoard);
-        scroller.setSize("800px", "600px");
-        dashboard.add(scroller, DockPanel.CENTER);
+        buildDashboard();
 
         initWidget(uiBinder.createAndBindUi(this));
     }
 
+    private void buildDashboard() {
+        dashboard = new DockPanel();
+        dashboard.setSpacing(4);
+        dashboard.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+
+        dashboard.add(new Menu(), DockPanel.NORTH);
+        dashboard.add(new Filters(), DockPanel.WEST);
+
+        final BookBoard bookBoard = new BookBoard();
+        final ScrollPanel scroller = new ScrollPanel(bookBoard);
+        scroller.setSize("800px", "600px");
+        dashboard.add(scroller, DockPanel.CENTER);
+    }
+
     public void showBooks() {
+        dashboard.setVisible(true);
+        graphs.setVisible(false);
     }
 
     public void showGraphs() {
+        dashboard.setVisible(false);
+        graphs.setVisible(true);
     }
 
 }
