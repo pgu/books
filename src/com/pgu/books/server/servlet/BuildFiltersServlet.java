126230b06f1fceace36fe7cf2b7dbef478b69a54
Guilcher Pascal <pgu.dev@gmail.com>
2012-04-08 12:42:50 +0200

    ..

--- src/com/pgu/books/server/servlet/BuildFiltersServlet.java
+++ src/com/pgu/books/server/servlet/BuildFiltersServlet.java
@@ -10,7 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.QueryResultIterator;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
 import com.google.gwt.user.client.rpc.IsSerializable;
+import com.googlecode.objectify.Query;
 import com.pgu.books.server.access.DAO;
 import com.pgu.books.server.domain.AuthorFilter;
 import com.pgu.books.server.domain.CategoryFilter;
@@ -22,9 +27,7 @@ public class BuildFiltersServlet extends HttpServlet {
 
     private static final Logger LOGGER = Logger.getLogger(BuildFiltersServlet.class.getName());
 
-    @SuppressWarnings("unchecked")
-    private static final List<Class<? extends IsSerializable>> FILTER_CLASSES = Arrays.asList(AuthorFilter.class,
-            EditorFilter.class, CategoryFilter.class);
+    private static final long LIMIT_MS = 1000 * 25;
 
     private final DAO dao = new DAO();
 
@@ -35,18 +38,21 @@ public class BuildFiltersServlet extends HttpServlet {
         doPost(req, resp);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException,
             IOException {
 
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Starting building filters");
+        final long startTime = System.currentTimeMillis();
 
-        // TODO PGU voir queue task
+        // clean filters
+        for (final Class<? extends IsSerializable> clazz : Arrays.asList(AuthorFilter.class, EditorFilter.class,
+                CategoryFilter.class)) {
 
-        // clean filters // TODO PGU cursor
-        for (final Class<? extends IsSerializable> clazz : FILTER_CLASSES) {
-            dao.ofy().delete(dao.ofy().query(clazz).listKeys());
+            final Queue queue = deleteFilter(clazz, startTime);
+            if (queue != null) {
+                return;
+            }
         }
 
         // loop through all books // TODO PGU cursor
@@ -58,6 +64,29 @@ public class BuildFiltersServlet extends HttpServlet {
             putFilterCategory(book);
         }
 
+    }
+
+    private <T extends IsSerializable> Queue deleteFilter(final Class<T> clazz, final long startTime) {
+
+        final int count = dao.ofy().query(clazz).count();
+        if (count > 0) {
+
+            final Query<T> q = dao.ofy().query(clazz);
+            final QueryResultIterator<T> itr = q.iterator();
+
+            while (itr.hasNext()) {
+
+                dao.ofy().delete(itr.next());
+
+                if (System.currentTimeMillis() - startTime > LIMIT_MS) {
+
+                    final Queue queue = QueueFactory.getQueue("buildFilters");
+                    queue.add(TaskOptions.Builder.withUrl("/buildFilters"));
+                    return queue;
+                }
+            }
+        }
+        return null;
     }
 
     private void putFilterCategory(final Book book) {
