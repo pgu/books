1c123f9b8e1557d1c6a4d4a87b5af87384623490
Pascal Guilcher <guilcher.p@sfeir.com>
2012-04-13 13:48:24 +0200

    ..

---
+++ src/com/pgu/books/server/exception/ProcessException.java
@@ -0,0 +1,10 @@
+package com.pgu.books.server.exception;
+
+@SuppressWarnings("serial")
+public class ProcessException extends Exception {
+
+    public ProcessException(final String message) {
+        super(message);
+    }
+
+}
