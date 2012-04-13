8371ea76c6f0f450d35c601f421cc318898a55eb
Guilcher Pascal <pgu.dev@gmail.com>
2012-04-14 00:13:00 +0200

    ..

--- src/com/pgu/books/server/domain/LetterFilter.java
+++ src/com/pgu/books/server/domain/LetterFilter.java
@@ -1,14 +1,62 @@
 package com.pgu.books.server.domain;
 
+import javax.persistence.Id;
+
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public abstract class LetterFilter implements IsSerializable {
 
+    @Id
+    private Long id;
+
     private String letter;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (letter == null ? 0 : letter.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LetterFilter other = (LetterFilter) obj;
+        if (letter == null) {
+            if (other.letter != null) {
+                return false;
+            }
+        } else if (!letter.equals(other.letter)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "LetterFilter [id=" + id + ", letter=" + letter + "]";
+    }
 
     public LetterFilter letter(final String letter) {
         setLetter(letter);
         return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
     }
 
     public String getLetter() {
