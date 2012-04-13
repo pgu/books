b54d7331346ca09c87b20f033c3f5009a9512e03
Guilcher Pascal <pgu.dev@gmail.com>
2012-04-14 00:09:50 +0200

    ..

---
+++ src/com/pgu/books/server/utils/ParserRequest.java
@@ -0,0 +1,11 @@
+package com.pgu.books.server.utils;
+
+import java.util.List;
+
+public class ParserRequest {
+
+    public String paramName;
+    public List<String> references;
+    public String defaultValue;
+
+}
